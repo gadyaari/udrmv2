c9e79be78e8172d45e95a4b3d975076acca974e9
root <root@localhost.localdomain>
2016-06-16 12:47:19 +0300

    add OTT side entitlement #time 1d

--- tests/test_genericEntitlement.js
+++ tests/test_genericEntitlement.js
@@ -6,6 +6,7 @@ require('util');
 
 require('../lib/utils/KalturaConfig');
 var OVP = require('../lib/entitlement/OVPEntitlement');
+var OTT = require('../lib/entitlement/OTTEntitlement');
 var MOCK = require('../lib/entitlement/GenericEntitlementMock');
 
 var WRONG_DURATION_MESSAGE = "wrong duration!";
@@ -13,8 +14,11 @@ var WRONG_ABSOLUTE_DURATION_MESSAGE = "wrong absolute_duration!";
 var WRONG_POLICY_MESSAGE = "wrong absolute_duration!";
 
 var KS = KalturaConfig.config.udrm.KS;
-var SERVER = KalturaConfig.config.udrm.OVPSERVER;
-console.log("Starting tests with IP: " + SERVER);
+var OVPSERVER = KalturaConfig.config.udrm.OVPSERVER;
+var OTTSERVER = KalturaConfig.config.udrm.OTTSERVER;
+var OTTPATH = KalturaConfig.config.udrm.OTTPATH;
+console.log("Starting tests with IP: " + OVPSERVER + " for OVP and " + OTTSERVER + " for OTT");
+
 
 
 //this test is just to see that the file is running and OK
@@ -30,7 +34,7 @@ exports.testOVPEntitlement = function(test) {
     test.expect(3);
 
     var customData = {"ca_system":"OVP","user_token":KS,"account_id":1982551,"content_id":"0_wsgv6s7h_0_sxsts283,0_wsgv6s7h_0_koa2swtn","files":"","udid":"6f541a81fb2d7328"}
-    var ovp = new OVP(SERVER,customData);
+    var ovp = new OVP(OVPSERVER,customData);
 
     var entitlement = ovp.getEntitlement();
     entitlement.then(
@@ -48,12 +52,39 @@ exports.testOVPEntitlement = function(test) {
     entitlement.finally(function(){test.done();});
 };
 
+
+// this test create OTTEntitlement and send requet for OTT server
+// answer is {0,0,None} because there is no such entryID
+exports.testOTTEntitlement = function(test) {
+    test.expect(3);
+
+    var customData = {"ca_system":"OTT","user_token":KS,"account_id":"","content_id":"1_6ojmokuo_1_73eztmlo,1_6ojmokuo_1_00n1ga1a,1_6ojmokuo_1_pls78sme,1_6ojmokuo_1_xg79vol6","files":"","udid":""};
+    var providerData = {"provider_sign_key":"wSeqVbdA31+nWm9OfBbseuIhi5LwJoRlpiwCyJz7HYg=","cas_password":"11111","key":"b1Wnx49V07a1IafqvYRbVcUsD5F7bj26Mkbt4MzAtBU=","iv":"h3jAqQOGlps/swgz5b0Kww==","cas_username":"conditionalaccess_198","provider":"kaltura"};
+    var ott = new OTT(OTTSERVER, OTTPATH, customData, providerData);
+
+    var entitlement = ott.getEntitlement();
+    entitlement.then(
+        function(result)
+        {
+            test.ok(result.duration == 0, WRONG_DURATION_MESSAGE);
+            test.ok(result.absolute_duration == "00:00:00", WRONG_ABSOLUTE_DURATION_MESSAGE);
+            test.ok(result.policy == "", WRONG_POLICY_MESSAGE);
+        },
+        function(err)
+        {
+            test.ok(false, err);
+        }
+    );
+    entitlement.finally(function(){test.done();});
+};
+
+
 //this test the OVPEntitlement in Mock condition
 //he check for getting back the default value define as static in OVPMockEntitlement
 exports.testOVPMockEntitlement = function(test) {
     test.expect(3);
     var customData = {"ca_system":"OVP","user_token":"","account_id":1982551,"content_id":"0_wsgv6s7h_0_sxsts283,0_wsgv6s7h_0_koa2swtn","files":"","udid":"6f541a81fb2d7328"}
-    var mock = new MOCK(SERVER,customData);
+    var mock = new MOCK(OVPSERVER,customData);
 
     var entitlement = mock.getEntitlement();
     entitlement.then(
@@ -76,7 +107,7 @@ exports.testOVPMockEntitlement = function(test) {
 exports.testOTTMockEntitlement = function(test) {
     test.expect(3);
     var customData = {"ca_system":"OTT","user_token":"","account_id":1982551,"content_id":"0_wsgv6s7h_0_sxsts283,0_wsgv6s7h_0_koa2swtn","files":"","udid":"6f541a81fb2d7328"}
-    var mock = new MOCK(SERVER,customData);
+    var mock = new MOCK(OTTSERVER,customData);
 
     var entitlement = mock.getEntitlement();
     entitlement.then(
