8faba73f686c78f76f230d52313e934e543f602b
Noam Arad <noam.arad@kaltura.com>
2016-07-04 10:51:03 +0300

    add playready tests and move parameters of test to config file

--- tests/test_widevine.js
+++ tests/test_widevine.js
@@ -1,70 +1,79 @@
-var util = require('util');
-var widevine = require('../lib/managers/KalturaWidevineManager');
+const util = require('util');
+const widevine = require('../lib/managers/KalturaWidevineManager');
 const Promise = require('bluebird');
 
-exports.testSomething = function(test) {
+exports.testSomething = function (test)
+{
     test.expect(1);
-    test.ok(true, "this assertion should pass");
+    test.ok(true, 'this assertion should pass');
     test.done();
 };
 
 exports.testWidevine =
 {
-    setUp: function (callback) {
+    setUp: function (callback)
+    {
         //KalturaCouchbaseConnector.getInstance();
         callback();
     },
 
-    tearDown: function (callback) {
-        KalturaLogger.log("tearDown is called");
+    tearDown: function (callback)
+    {
+        KalturaLogger.log('tearDown is called');
         callback();
     },
-    
-    encryption:function(test)
+
+    encryption: function (test)
     {
         test.expect(8);
-        var providerData = {
-            key: 'b1Wnx49V07a1IafqvYRbVcUsD5F7bj26Mkbt4MzAtBU=',
-            iv: 'h3jAqQOGlps/swgz5b0Kww==',
-            provider: 'kaltura',
-            provider_sign_key: 'jernkVsZ6j3LheMnStCAVmSncyXDBBmVSGfcWr1WQAA='
+        const providerData = {
+            key: KalturaConfig.config.tests.widevine.key,
+            iv: KalturaConfig.config.tests.widevine.iv,
+            provider: KalturaConfig.config.tests.widevine.provider,
+            provider_sign_key: KalturaConfig.config.tests.widevine.provider_sign_key,
         };
-    
-        var postParams = {
-            "files": "0_cftajt0x",
-            "content_id": "0_g6d82oug",
-            "ca_system": "ovp",
-            "account_id": "2621"
+
+        const postParams = {
+            files: KalturaConfig.config.tests.widevine.files,
+            content_id: KalturaConfig.config.tests.widevine.content_id,
+            ca_system: KalturaConfig.config.tests.widevine.ca_system,
+            account_id: KalturaConfig.config.tests.widevine.account_id,
         };
+
 
         function success(result)
         {
-            //console.log("@@NA got result ["+util.inspect(result)+"]");
-            for (let resKey=0; resKey<result.length; resKey++) 
+            //console.log(`@@NA got result [${util.inspect(result)}]`);
+            for (let resKey = 0; resKey < result.length; resKey++)
             {
-                test.equal(result[resKey][0].key_id, "ongd6sHs/QaD1tk2RbYsMg==");
-                test.equal(result[resKey][0].key, "vYSe0cNYP1gMfaG80yiIpg==");
-                test.equal(result[resKey][0].pssh[0].uuid, "edef8ba9-79d6-4ace-a3c8-27dcd51d21ed");
-                test.equal(result[resKey][0].pssh[0].data, "CAESEKJ4HerB7P0Gg9bZNkW2LDIaB2thbHR1cmEiCjBfZzZkODJvdWcqBVNEX0hE");
+                test.equal(result[resKey][0].key_id, KalturaConfig.config.tests.widevine.expected_keyId);
+                test.equal(result[resKey][0].key, KalturaConfig.config.tests.widevine.expected_key );
+                test.equal(result[resKey][0].pssh[0].uuid, KalturaConfig.config.tests.widevine.expected_uuid);
+                test.equal(result[resKey][0].pssh[0].data, KalturaConfig.config.tests.widevine.expected_data);
+
             }
-        };
+        }
         function notScucess(error)
         {
             //console.log("@@NA got error "+util.inspect(error));
             test.ok(false, JSON.stringify(error));
-        };
+        }
 
-        var wvManager = new widevine.KalturaWidevineManager();
-        var encryptionDataPromise = wvManager.createEncryptionData(providerData, postParams);
-        
+        const wvManager = new widevine.KalturaWidevineManager();
+        const encryptionDataPromise = wvManager.createEncryptionData(providerData, postParams);
+
         //encryptionDataPromise.then(success,notScucess);
-        providerData.seed = 'rJmT2nTj7acd99aNpVjDYuRPs8tHGt6q80yWpaLA'
-        var encryptionDataPromise2 = wvManager.createEncryptionData(providerData, postParams);
+        providerData.seed = KalturaConfig.config.tests.widevine.seed;
+        const encryptionDataPromise2 = wvManager.createEncryptionData(providerData, postParams);
         //encryptionDataPromise2.then(success,notScucess);
-        
-        let allPromises = Promise.all([encryptionDataPromise, encryptionDataPromise2]);
+
+        const allPromises = Promise.all([encryptionDataPromise, encryptionDataPromise2]);
         allPromises.then(success, notScucess);
-        allPromises.finally(function(){test.done();});
-        
-    }
+        allPromises.finally(
+            function ()
+            {
+                test.done();
+            }
+        );
+    },
 };
